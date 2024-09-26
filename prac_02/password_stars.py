aa424d653382e9bfbf64d257dc4548c594461722
jc972271 <liam.whiting@my.jcu.edu.au>
2024-09-27 09:44:10 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,26 @@
 CP1404 - Practical 2
 Code for inputting password
 """
-password_length = 8
-password = input('Enter your password: ')
 
-while len(password) < password_length:
-    print(f"Password must be longer then {password_length} characters")
+
+def main():
+    password_length = 8
+
+    password = get_password(password_length)
+
+    obfuscate_password(password)
+
+
+def get_password(password_length):
     password = input('Enter your password: ')
+    while len(password) < password_length:
+        print(f"Password must be longer then {password_length} characters")
+        password = input('Enter your password: ')
+    return password
 
-print("*" * len(password))
+
+def obfuscate_password(password):
+    print("*" * len(password))
+
+
+main()
