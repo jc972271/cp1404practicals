676592154f0d63439bffc77eb08c25057ef3a2e0
jc972271 <liam.whiting@my.jcu.edu.au>
2024-09-27 10:07:05 +1000

    Refactor score code to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,29 @@
 CP1404 - Practical 2
 Code to determine score status
 """
+from random import randint
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    evaluation = evaluate_score(score)
+    print(evaluation)
+
+    random_score = randint(0, 100)
+    evaluation = evaluate_score(random_score)
+    print("Random Score:", random_score)
+    print(evaluation)
+
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        evaluation = "Invalid score"
+    elif score >= 90:
+        evaluation = "Excellent"
+    elif score >= 50:
+        evaluation = "Passable"
+    else:
+        evaluation = "Bad"
+    return evaluation
+
+
+main()
