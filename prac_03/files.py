dc07021e6923ffd0987dddd1ad1afa574775d107
jc972271 <liam.whiting@my.jcu.edu.au>
2024-10-04 11:04:31 +1000

    Add open and read numbers.txt and add first 2 lines task to code

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,10 +5,15 @@ various file interactions
 
 name = input("Name: ")
 FILENAME = "name.txt"
-out_file = open(FILENAME, "w")
-print(name, file=out_file)
-out_file.close()
+out_name_file = open(FILENAME, "w")
+print(name, file=out_name_file)
+out_name_file.close()
 
-in_file = open(FILENAME, "r")
-print(f"Hi, {in_file.read().strip()}!")
+in_name_file = open(FILENAME, "r")
+print(f"Hi, {in_name_file.read().strip()}!")
+in_name_file.close()
 
+with open("numbers.txt", "r") as numbers_file:
+    x = int(numbers_file.readline())
+    y = int(numbers_file.readline())
+    print(x + y)
