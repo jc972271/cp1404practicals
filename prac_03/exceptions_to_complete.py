ed34efdd93a9b4efe800fcb6b6ec97f6611ff92f
jc972271 <liam.whiting@my.jcu.edu.au>
2024-10-04 10:32:41 +1000

    Add exceptions_to_complete code

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,14 @@
+"""
+CP1404- Practical 3
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+    else:
+        is_finished = True
+print("Valid result is:", result)  # result will be defined
