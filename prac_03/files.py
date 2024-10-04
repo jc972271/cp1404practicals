e7e22638d601c60c54b1ca2beed6060289183371
jc972271 <liam.whiting@my.jcu.edu.au>
2024-10-04 10:57:33 +1000

    Add reading file task to code

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,4 +7,8 @@ name = input("Name: ")
 FILENAME = "name.txt"
 out_file = open(FILENAME, "w")
 print(name, file=out_file)
+out_file.close()
+
+in_file = open(FILENAME, "r")
+print(f"Hi, {in_file.read().strip()}!")
 
