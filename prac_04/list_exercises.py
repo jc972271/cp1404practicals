70750ed3af435f64b396f1c9ed21adba89c59778
jc972271 <liam.whiting@my.jcu.edu.au>
2024-10-11 10:27:08 +1000

    Add username checker to list exercise code

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,10 @@ print(f"The last number is {numbers[len(numbers) - 1]}")
 print(f"The lowest number is {min(numbers)}")
 print(f"The highest number is {max(numbers)}")
 print(f"The average number is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+if input("Username: ") in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
