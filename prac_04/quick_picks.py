40a4e088784572c3ef1ae62d75f2a6cec110b8c8
jc972271 <liam.whiting@my.jcu.edu.au>
2024-10-12 17:49:09 +1000

    Replace hard coded integers with constants

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,24 +1,22 @@
 """
 CP1404 - Practical 4
-
+Code to generate "quick picks" each line consists of 6 random numbers between 1 and 45.
 """
 
 from random import randint
 
+NUMBERS_PER_LINE = 6
+MAXIMUM = 45
+MINIMUM = 1
+
 number_of_picks = int(input("How many quick picks do you want: "))
-quick_picks = []
 
 for i in range(number_of_picks):
     quick_pick = []
-
-    while len(quick_pick) < 6:
-        number = randint(1, 45)
+    while len(quick_pick) < NUMBERS_PER_LINE:
+        number = randint(MINIMUM, MAXIMUM)
         if not number in quick_pick:
             quick_pick.append(number)
-
     quick_pick.sort()
-    for j in range(6):
-        print(f"{quick_pick[i]:>2}", end=" ")
-    print()
 
-    quick_picks.append(quick_pick)
+    print(" ".join(f"{number:2}" for number in quick_pick))
