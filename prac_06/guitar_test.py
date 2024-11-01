290f0bf99a63a25b2598a472b9bccba365c88a58
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-01 10:05:49 +1000

    Add the testing code for the Guitar class to prac 6

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical 6 -
+Testing the Guitar class
+"""
+from guitar import Guitar
+
+CURRENT_YEAR=2024
+
+fender = Guitar("Fender Stratocaster",2014,765.40)
+gibson = Guitar("Gibson L-5 CES",1922,16035.40)
+
+print(f"{gibson.name} get_age() - Expected 102. Got {gibson.get_age(CURRENT_YEAR)}")
+print(f"{fender.name} get_age() - Expected 10. Got {fender.get_age(CURRENT_YEAR)}")
+
+print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage(CURRENT_YEAR)}")
+print(f"{fender.name} is_vintage() - Expected False. Got {fender.is_vintage(CURRENT_YEAR)}")
