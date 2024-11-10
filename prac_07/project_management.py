69571795b549677c263abf6dba01a723e2ca2397
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-10 14:02:27 +1000

    Add load file function to project_management.py in prac 7

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,7 @@ Project Manager
 Estimate: 40 minutes
 Actual:
 """
+from unittest.mock import DEFAULT
 
 MENU = """- (L)oad projects  
 - (S)ave projects  
@@ -13,14 +14,20 @@ MENU = """- (L)oad projects
 - (U)pdate project
 - (Q)uit"""
 
+DEFAULT_FILE = "projects.txt"
+
 def main():
     print("Welcome to Pythonic Project Management")
+    projects = load_project()
+    print(f"{len(projects)} projects loaded from {DEFAULT_FILE}")
     print(MENU)
     choice = input(">>> ").upper()
     print(choice)
     while choice != "Q":
         if choice == "L":
-            pass
+            print("Load Project")
+            filename = input("Filename:")
+            load_project(filename)
         elif choice == "S":
             pass
         elif choice == "D":
@@ -35,4 +42,14 @@ def main():
             print("Invalid choice")
         print(MENU)
         choice = input(">>> ").upper()
+
+def load_project(filename=DEFAULT_FILE):
+    infile = open(filename, "r")
+    projects = []
+    infile.readline() #remove headings
+    for line in infile:
+        line = line.strip().split("\t")
+        projects.append(line)
+    infile.close()
+    return projects
 main()
