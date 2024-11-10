6feb3ab06b1c1f712f90cd391330125a22d82601
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-10 13:43:04 +1000

    Add basic project class

---
+++ prac_07/project.py
@@ -0,0 +1,11 @@
+"""Project Class"""
+class Project:
+    def __init__(self, name, start_date, priority, cost, completion):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost:.2f}, completion: {self.completion}%"
