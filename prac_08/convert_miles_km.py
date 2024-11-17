4f858fe9606ad18bfe32190bf9f10a88c5bed159
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-17 21:27:10 +1000

    Add the calculation function for convert_miles_km code in prac 8

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,14 +7,25 @@ Started 17/11/2024
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.609344
 
 class ConvertMilesToKilometersApp(App):
     """ ConvertMilesToKilometersApp is a Kivy App for converting miles to kilometers"""
+    output_kilometers = StringProperty()
+
     def build(self):
         """ build the Kivy app from the kv file """
         self.title = "Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def calculate_result(self):
+        value = float(self.root.ids.input_miles.text)
+        result = value * MILES_TO_KM
+        self.output_kilometers = str(result)
+
 
 
 ConvertMilesToKilometersApp().run()
