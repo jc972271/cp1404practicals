3fd2c19357ad87ee319a70049732653a960df75e
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-24 21:53:17 +1000

    Add the unreliable car class and its testing to prac 9

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -4,7 +4,6 @@ Car class
 """
 from prac_09.car import Car
 
-
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
     price_per_km = 1.23
---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+Unreliable Car class
+"""
+from random import randint
+
+from prac_09.car import Car
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes a reliability."""
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent Car but check the car starts"""
+        if randint(0,100) < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = super().drive(0)
+        return distance_driven
