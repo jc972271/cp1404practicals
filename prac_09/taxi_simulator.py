62aa1f50487cab7c6932de60cfd7be104ddb057c
jc972271 <liam.whiting@my.jcu.edu.au>
2024-11-25 10:17:03 +1000

    Add choose taxi function to taxi simulator in prac 9

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,20 +1,41 @@
 
 
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
 
 MENU = """q)uit, c)hoose taxi, d)rive"""
 
 def main():
+    chosen_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
     print("Let's Drive!")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            pass
+            chosen_taxi = choose_tax(taxis)
+            print(type(chosen_taxi))
         elif choice == "D":
             pass
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>> ").upper()
+
+def choose_tax(taxis):
+    print("Taxis available:")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+    try:
+        taxi_index = int(input("Choose Taxi: "))
+        if taxi_index > len(taxis) - 1:
+            print("Invalid place number")
+        elif taxi_index < 0:
+            print("Number must be > 0")
+        else:
+            return taxis[taxi_index]
+    except ValueError:
+        print("Invalid taxi choice")
 
 main()
